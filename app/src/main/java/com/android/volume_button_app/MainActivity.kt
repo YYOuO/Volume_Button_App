f8e6af6c3067d023758723ac959e592b3cd65d5c
YYOuO <eddy101173@gmail.com>
2023-09-08 22:43:46 +0800

    Percentage change with volume

--- app/src/main/java/com/android/volume_button_app/MainActivity.kt
+++ app/src/main/java/com/android/volume_button_app/MainActivity.kt
@@ -3,21 +3,29 @@ package com.android.volume_button_app
 import android.app.StatusBarManager
 import android.content.ComponentName
 import android.content.Context
+import android.content.IntentFilter
 import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.Wallpapers
+import androidx.compose.ui.unit.dp
 import com.android.volume_button_app.ui.theme.Volume_button_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,45 +39,38 @@ class MainActivity : ComponentActivity() {
 					val icon = Icon.createWithResource(this , R.drawable.ic_launcher_background)
 					statusBarManager.requestAddTileService(componentName , "Volume_Btn" , icon , {} , {})
 				}
-
+				// Register BroadCastReceiver
+				val receiver = VolumeMonitor()
+				val filter = IntentFilter()
+				filter.addAction("android.media.VOLUME_CHANGED_ACTION")
+				registerReceiver(receiver , filter)
 			}
 		}
 	}
 }
 
-//@Composable
-//fun Greeting(name : String , modifier : Modifier = Modifier , func : Unit) {
-//	Column {
-//		Text(
-//			text = "Hello $name!" , modifier = modifier
-//		)
-//		Button(modifier = Modifier.padding(15.dp) , onClick = { func })
-//	}
-//
-//}
-
-//@Preview(showBackground = true , wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE)
-//@Composable
-//fun GreetingPreview() {
-//	Volume_button_appTheme {
-//		Greeting("Android")
-//	}
-//}
-
 @Preview(showBackground = true , wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE)
 @Composable
 fun MyApp() {
 	Volume_button_appTheme {
+
 		Card() {
 			Column {
 				Row(verticalAlignment = Alignment.CenterVertically) {
-					Text(text = "This is a text")
+					Text(text = "This is a text  ")
 					Switch(checked = true , onCheckedChange = {})
 				}
 				Divider()
 				Row(verticalAlignment = Alignment.CenterVertically) {
 					Text(text = "This is a text")
 					Slider(value = 0.5f , onValueChange = {})
+				}
+				FloatingActionButton(
+					onClick = { /*TODO*/ } ,
+					modifier = Modifier
+						.padding(16.dp)
+				) {
+					Icon(Icons.Filled.Add , "Add qs")
 				}
 			}
 		}
--- app/src/main/java/com/android/volume_button_app/VolumeTileService.kt
+++ app/src/main/java/com/android/volume_button_app/VolumeTileService.kt
@@ -1,17 +1,17 @@
 package com.android.volume_button_app
 
 import android.content.Context
+import android.content.Intent
 import android.media.AudioManager
 import android.service.quicksettings.TileService
+import android.util.Log
 
 class VolumeTileService : TileService() {
-	override fun onStartListening() {
-		val audioManager : AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-		val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-		val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
-		val volumePercentage = ((currentVolume.toFloat() / maxVolume.toFloat()) * 100).toInt()
-		qsTile.subtitle = "$volumePercentage%"
-		qsTile.updateTile()
+	override fun onStartCommand(intent : Intent? , flags : Int , startId : Int) : Int {
+		Log.i("test" , "receive command")
+		volumePercentage()
+		checkQSActiveAndIcon()
+		return super.onStartCommand(intent , flags , startId)
 	}
 
 	// Replace as Volume Button
@@ -19,5 +19,18 @@ class VolumeTileService : TileService() {
 		super.onClick()
 		val audioManager : AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
 		audioManager.adjustVolume(AudioManager.ADJUST_SAME , AudioManager.FLAG_SHOW_UI)
+	}
+
+	private fun checkQSActiveAndIcon() {
+	}
+
+	private fun volumePercentage() {
+		val audioManager : AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+		val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+		val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+		val volumePercentage = ((currentVolume.toFloat() / maxVolume.toFloat()) * 100).toInt()
+		qsTile.subtitle = "$volumePercentage%"
+		qsTile.updateTile()
+		Log.i("test" , "percentagechanged")
 	}
 }
