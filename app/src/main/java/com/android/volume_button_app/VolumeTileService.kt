fe81f599921fae40b500bc164575fb99ea7e96d7
YYOuO <eddy101173@gmail.com>
2023-10-05 00:29:21 +0800

    Display source and status on tile ( not in real-time )

--- app/src/main/java/com/android/volume_button_app/VolumeTileService.kt
+++ app/src/main/java/com/android/volume_button_app/VolumeTileService.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.graphics.drawable.Icon
+import android.media.AudioDeviceInfo
 import android.media.AudioManager
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
@@ -13,37 +14,44 @@ class VolumeTileService : TileService() {
 	private val receiver : BroadcastReceiver = object : BroadcastReceiver() {
 		override fun onReceive(context : Context? , intent : Intent) {
 			if (intent.action.equals("android.media.VOLUME_CHANGED_ACTION")) {
-				val newVolume = intent.getIntExtra("android.media.EXTRA_VOLUME_STREAM_VALUE" , 0)
-				val oldVolume = intent.getIntExtra("android.media.EXTRA_PREV_VOLUME_STREAM_VALUE" , 0)
-				if (newVolume != oldVolume) {
-					updateQuickSettingsTile()
-				}
+				updateQuickSettingsTile()
 			}
 		}
 	}
 
-	// Replace as Volume Button
-	override fun onClick() {
-		super.onClick()
-		val audioManager : AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-		audioManager.adjustVolume(AudioManager.ADJUST_SAME , AudioManager.FLAG_SHOW_UI)
+	// Original plan was to use communicationDeviceChangeListener but failed
+	override fun onStartListening() {
+		super.onStartListening()
+		updateQuickSettingsTile()
 		val filter = IntentFilter()
 		filter.addAction("android.media.VOLUME_CHANGED_ACTION")
 		registerReceiver(receiver , filter)
 	}
 
-	private val muteIcon : Icon = Icon.createWithResource("com.example.myapplication" , R.drawable.mute)
-	private val unMuteIcon : Icon = Icon.createWithResource("com.example.myapplication" , R.drawable.unmute)
+	override fun onStopListening() {
+		super.onStopListening()
+		unregisterReceiver(receiver)
+	}
+
+	override fun onClick() {
+		super.onClick()
+		val audioManager : AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+		audioManager.adjustVolume(AudioManager.ADJUST_SAME , AudioManager.FLAG_SHOW_UI)
+		updateQuickSettingsTile()
+	}
+
 	private fun updateQuickSettingsTile() {
 		val audioManager : AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-		val muteState : Boolean = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC) == 0
-		if (muteState) {
-			qsTile.icon = muteIcon
+		if (audioManager.getStreamVolume(AudioManager.STREAM_MUSIC) == 0) {
+			qsTile.icon = Icon.createWithResource("com.android.volume_button_app" , R.drawable.mute)
 			qsTile.state = Tile.STATE_INACTIVE
 		}
 		else {
-			qsTile.icon = unMuteIcon
+			qsTile.icon = Icon.createWithResource("com.android.volume_button_app" , R.drawable.unmute)
 			qsTile.state = Tile.STATE_ACTIVE
+		}
+		if (audioManager.communicationDevice?.type != AudioDeviceInfo.TYPE_BUILTIN_EARPIECE) {
+			qsTile.icon = Icon.createWithResource("com.android.volume_button_app" , R.drawable.ic_launcher_foreground)
 		}
 		val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
 		val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
